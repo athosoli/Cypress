9b3019e2cf6393c039de577b18012fb30efbbcb5
Athos-Oliveira <athos.udk@gmail.com>
2021-09-28 23:11:14 -0300

    ok

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -4,8 +4,8 @@ login:{
     password:'#inputPassword'
 },
 descricao:{
-    criar:'CYTESTE',
-    editado:'CYEDITADO',
+    criar:'PastorBelga',
+    editado:'Doberman',
    
 
 },
@@ -14,17 +14,30 @@ menu:{
     fixar:'.fold-toggle',
     adicionar:'#btn-adicionar',
     caminho:':nth-child(3) > .ng-binding',
+    caminho2:':nth-child(4) > .ng-binding',
     atualiza:'.layout-row > :nth-child(1) > .md-raised > .fa',
     aviso:'.toast-message',
     fechaaviso:'.toast > .md-icon-button > .ng-scope',
+    painel:'.panel',
+
 },
 integracao:{
     icone:':nth-child(1) > [href="#"] > .fi',
     cliente:'[style=""] > ul.ng-scope > :nth-child(1) > .navigation-menu-item > .navigation-menu-button > .title',
     grupoCliente: '[style=""] > ul.ng-scope > :nth-child(2) > .navigation-menu-item > .navigation-menu-button > .title',
     grupoFornecedor: ':nth-child(1) > :nth-child(2) > ul.ng-scope > :nth-child(2) > .navigation-menu-item > .navigation-menu-button > .title',
+    grupoEmpresarial: ':nth-child(4) > ul.ng-scope > .navigation-menu-node > .navigation-menu-item > .navigation-menu-button > .title',
     fornecedor: ':nth-child(1) > :nth-child(2) > ul.ng-scope > :nth-child(1) > .navigation-menu-item > .navigation-menu-button > .title',
-    adicionar:'#btn-adicionar > span.ng-scope'
+    adicionar:'#btn-adicionar > span.ng-scope',
+    Visualizar:'[fields="ctrl.dicionarios[0]"] > :nth-child(1) > :nth-child(1)'
+},
+faturamento:{
+    icone:':nth-child(5) > [href="#"] > .fi',
+    documentoSaida:':nth-child(1) > :nth-child(4) > ul.ng-scope > :nth-child(3) > .navigation-menu-item > .navigation-menu-button > .title',
+    pedido:':nth-child(1) > :nth-child(4) > ul.ng-scope > :nth-child(2) > .navigation-menu-item > .navigation-menu-button > .title',
+    orçamento:':nth-child(1) > :nth-child(4) > ul.ng-scope > :nth-child(1) > .navigation-menu-item > .navigation-menu-button > .title',
+    caminho:'.breadcrumb > :nth-child(4) > .ng-binding',
+    adicionar:'[ng-click="ctrl.adicionarCadastro()"]'
 
     
 },
